2829e1065ddaa3beba0e358e184da47b6e3bddc0
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2025-09-19 22:12:38 -0300

    fix

--- Escenas/nivel_test_cosas.gd
+++ Escenas/nivel_test_cosas.gd
@@ -1,15 +1,10 @@
 extends Node3D
 
 @onready var camara_ver_tele : Camera3D= %CameraTele
-var animacion_ver_tele : bool = false
-var target_camara_position : Vector3
 
 func _ready() -> void:
 	Global.poner_vhs_tv.connect(_on_poner_vhs)
 
-func _physics_process(delta: float) -> void:
-	if animacion_ver_tele:
-		camara_ver_tele.global_position = camara_ver_tele.global_position.lerp(target_camara_position, 0.05 * delta * 60)
 
 func _on_poner_vhs():
 	%AnimationTele.play("poner_vhs")
